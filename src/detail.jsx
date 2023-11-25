3001791f7baf871598a0bfa5748de89d11e40197
Dyala Muhammad <dyalamuhammad@gmail.com>
2023-11-25 13:20:14 +0700

    adjust mobile

--- src/detail.jsx
+++ src/detail.jsx
@@ -37,7 +37,7 @@ const Detail = () => {
                   </div>
                   <div className="quran-arti">{ayat.teksIndonesia}</div>
                 </div>
-              </div>
+               </div>
             ))}
         
       </div>
