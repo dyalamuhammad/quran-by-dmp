db8c4ea90ad1c71134b97a35b6ffed219404d06e
Dyala Muhammad <dyalamuhammad@gmail.com>
2023-11-25 22:15:26 +0700

    add title and logo

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Movie from "./movie";
 import Detail from "./detail";
+import { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    document.title = "Al-Qur'an by DMP";
+  }, []);
   
 
   return (
     <div className="App">
       <header className="App-header">
         <Router>
+          <head>
+            <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+          </head>
           <Routes>
             <Route path='/' element={<Home />}/>
             <Route path='/surah' element={<Movie />}/>
